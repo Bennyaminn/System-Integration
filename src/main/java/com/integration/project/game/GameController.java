15c225a3e4c3938f2304a552270a40352fbe6270
Beniamin <107195141+Bennyaminn@users.noreply.github.com>
2022-06-11 14:35:33 +0200

    Add files via upload

---
+++ src/main/java/com/integration/project/game/GameController.java
@@ -0,0 +1,58 @@
+package com.integration.project.game;
+
+import com.integration.project.json.MyJSON;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "api/game")
+public class GameController {
+
+    private final GameService gameService;
+
+    @Autowired
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    //Zwraca wszyskie gry - w przyszłosci ograniczyc projectem
+    @GetMapping
+    public List<Game> getAll(){
+        return gameService.getGames();
+    }
+
+
+    //Zwraca nulla jesli nie ma i opbiekt jesli jest
+    @GetMapping("/by")
+    public Optional<Game> getById(@RequestParam(name = "id", required = false) Integer id){
+            return id != null?gameService.getGameById(id):Optional.empty();
+    }
+
+    //to moje do testów
+    @GetMapping("test")
+    public String testowanie(){
+        return "testowanie";
+    }
+
+    //tutaj jest importowanie jsona do bazy - uwaga trochę trwa
+    @GetMapping("/json/import")
+    public String jsonImport(){
+        MyJSON.importData(gameService);
+        return "Zaimportowano";
+    }
+
+    //Tutaj eksportowanie do pliku json
+    @GetMapping("/json/export")
+    public String jsonExport(){
+        MyJSON.exportData(gameService);
+        return "Wyeksportowano";
+    }
+
+
+}
---
+++ src/main/java/com/integration/project/player/PlayerController.java
@@ -0,0 +1,43 @@
+package com.integration.project.player;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "api/player")
+public class PlayerController {
+    private final PlayerService playerService;
+
+    @Autowired
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    //Zwraca wszystkich graczy
+    @GetMapping
+    public List<Player> getAll() {return playerService.getPlayers();}
+
+    //Zwraca gracza po id lub po nzawie
+    @GetMapping("/by")
+    public List<Player> getById(@RequestParam(name = "id", required = false) Integer id,
+                                @RequestParam(name = "name", required = false) String name){
+
+        if(id != null){
+            return playerService.getPlayerById(id).stream().toList();
+        }
+        if(name != null){
+            System.out.println(name);
+            return playerService.getPlayersByName(name);
+        }
+        return List.of();
+    }
+
+
+
+}
